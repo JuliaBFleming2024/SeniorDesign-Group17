b73556bd7b868b3772d03a52ec77d185434b1905
rnoble06 <ryan.d.noble@outlook.com>
2024-02-06 17:41:13 -0500

    Start and End Time function fixed. Added pagination

--- Main.py
+++ Main.py
@@ -12,7 +12,6 @@ import re
 
 # Function to get event itinerary
 def get_event_itinerary( event_results_df, num_events, itin_time):
-    # FIXME - note: this will be a copy of the dataframe so we can drop rows when used. df.drop()
     events = 0
     while (events != num_events):
 
@@ -28,7 +27,6 @@ def get_event_itinerary( event_results_df, num_events, itin_time):
             upcoming_bin_df = event_results_df.head(3)
             event_itinerary_df = upcoming_bin_df.sample(1)
         else:
-            # FIXME create function to create new df for next random selection.
             upcoming_bin_df = upcoming_bin_df.sample(1)
             event_itinerary_df = pd.concat([event_itinerary_df, upcoming_bin_df], ignore_index=True)  # Concatenate and reset index
         
@@ -95,42 +93,50 @@ def get_start_end_times( event_time_list, event_rows ):
     event_time_start = initialize_list(event_rows)
     event_time_end   = initialize_list(event_rows)
 
-    try:
-        # split by comma
-        for x in range(event_rows):
-            dayTimeSplit = event_time_list[x].split(", ")
+    # split by comma
+    for x in range(event_rows):
+        dayTimeSplit = event_time_list[x].split(", ")
 
-            # No split, this is likely a multiday event, that does not have a time provided other than the days
-            if len(dayTimeSplit)    == 1:
-                event_time_start[x] = None
+        # Place None for events missing time info.
+        if len(dayTimeSplit)    == 1:
+            event_time_start[x] = None
+            event_time_end[x]   = None
+
+        # has 2 splits, first element is the day, 2nd element is the time.
+        elif len(dayTimeSplit)  == 2:
+            timeSplit = dayTimeSplit[1].split(" – ")
+            # if no split, it is likely missing an end time.
+            if len(timeSplit)    == 1:
+                timeSplit    = standardize_time(timeSplit)
+                timeSplit[0]        = datetime.datetime.strptime(timeSplit[0], "%I:%M %p")
+                timeSplit[0]        = timeSplit[0].strftime("%H:%M")
+                event_time_start[x] = timeSplit[0]
                 event_time_end[x]   = None
 
-            # has a split, first element is the day, 2nd element is the time.
-            elif len(dayTimeSplit)  == 2:
-                timeSplit = dayTimeSplit[1].split(" – ")
-                # if no split, it is likely missing an end time.
-                if len(timeSplit)    == 1:
-                    timeSplit[0]        = datetime.datetime.strptime(timeSplit[0], "%I:%M %p")
-                    timeSplit[0]        = timeSplit[0].strftime("%H:%M")
-                    event_time_start[x] = timeSplit[0]
-                    event_time_end[x]   = None
+            elif len(timeSplit)    == 2:
+                timeSplit    = standardize_time(timeSplit)
 
-                elif len(timeSplit)    == 2:
-                    timeSplit    = standardize_time(timeSplit)
+                timeSplit[0] = datetime.datetime.strptime(timeSplit[0], "%I:%M %p")
+                timeSplit[0] = timeSplit[0].strftime("%H:%M")
 
-                    timeSplit[0] = datetime.datetime.strptime(timeSplit[0], "%I:%M %p")
-                    timeSplit[0] = timeSplit[0].strftime("%H:%M")
+                timeSplit[1] = datetime.datetime.strptime(timeSplit[1], "%I:%M %p")
+                timeSplit[1] = timeSplit[1].strftime("%H:%M")
 
-                    timeSplit[1] = datetime.datetime.strptime(timeSplit[1], "%I:%M %p")
-                    timeSplit[1] = timeSplit[1].strftime("%H:%M")
+                event_time_start[x] = timeSplit[0]
+                event_time_end[x]   = timeSplit[1]
+        
+        # has 3 splits, 1st mmm dd, 2nd time - mmm dd, 3rd time
+        elif len(dayTimeSplit)  == 3:
+            # FIXME - if there are 2 times listed, use those as start and end. Should be easy, just using None until it gets fixed.
+            event_time_start[x] = None
+            event_time_end[x]   = None
 
-                    event_time_start[x] = timeSplit[0]
-                    event_time_end[x]   = timeSplit[1]
+        # otherwise, use None to at least have it show in the "missing time info" dataframe
+        else:
+            event_time_start[x] = None
+            event_time_end[x]   = None
 
-        return event_time_start, event_time_end
-
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    return event_time_start, event_time_end
 # End get_start_end_times
 
 
@@ -145,10 +151,23 @@ def get_when_from_df( event_time_list, event_rows ):
 
 
 
+# Function to initialize list
 def initialize_list( list_size ):
     new_list = [None] * list_size
     return new_list
 # End initialize_list
+
+
+
+# process date column to focus on times
+def process_dates( time_list ):
+    days    = re.compile('(Sun|Mon|Tue|Wed|Thu|Fri|Sat), ')
+    tz_abbr = re.compile(' (AST|EST|MST|ET|CT|UTC|EDT|WST|CST|PST|NST)')
+    for x in range(len(time_list)):
+        time_list[x] = days.sub('', time_list[x])
+        time_list[x] = tz_abbr.sub('', time_list[x])
+    return time_list
+# End process_dates
 
 
 
@@ -164,52 +183,61 @@ def separate_events_WO_times ( event_results_df ):
 
 # Function to adjust times to match %HH:%MM %p format from %HH or %HH %p
 def standardize_time( time2Adj ):
-    # place :00 for times without such as: 4, 4 AM, 4 PM
-    for y in range(2):
-        fixTime = time2Adj[y].split(":")
+    if (len(time2Adj) == 1):
+        fixTime = time2Adj[0].split(":")
         if ( len(fixTime) == 1 ):
-            if ( ("AM" not in time2Adj[y]) and ("PM" not in time2Adj[y]) ):
-                time2Adj[y] = time2Adj[y] + ":00"
+            ampmTime = fixTime[0].split(" ")
+            time2Adj[0] = ampmTime[0] + ":00 " + ampmTime[1]
+        return time2Adj
+                
+    elif (len(time2Adj) == 2):
+        # place :00 for times without such as: 4, 4 AM, 4 PM
+        for y in range(2):
+            fixTime = time2Adj[y].split(":")
+            if ( len(fixTime) == 1 ):
+                if ( ("AM" not in time2Adj[y]) and ("PM" not in time2Adj[y]) ):
+                    time2Adj[y] = time2Adj[y] + ":00"
+                else:
+                    ampmTime = fixTime[0].split(" ")
+                    time2Adj[y] = ampmTime[0] + ":00 " + ampmTime[1]
+        
+        if ( ("AM" not in time2Adj[0]) and ("PM" not in time2Adj[0]) ):
+            # Attach AM or PM to the start time, based on case
+            amORpm = time2Adj[1].split(" ")
+
+            time0 = int(time2Adj[0].split(":")[0])
+            time1 = int(amORpm[0].split(":")[0])
+
+            # Case 1: Noon or midnight is a start time.
+            if ( (time0 > time1) and (time0 == 12) ):
+                if amORpm[1] == "AM":
+                    time2Adj[0] = time2Adj[0] + " AM"
+                else:
+                    time2Adj[0] = time2Adj[0] + " PM"
+            
+            # Case 2: Noon or midnight is an end time.
+            elif ( (time0 < time1) and (time1 == 12) ):
+                if amORpm[1] == "AM":
+                    time2Adj[0] = time2Adj[0] + " PM"
+                else:
+                    time2Adj[0] = time2Adj[0] + " AM"
+            
+            # Case 3: transition period from AM to PM (short timeframes)
+            elif ( (amORpm[1] == "PM") and (time0 > time1) ):
+                time2Adj[0] = time2Adj[0] + " AM"
+            
+            # Case 4: transition period from PM to AM (short timeframes)
+            elif ( (amORpm[1] == "AM") and (time0 > time1) ):
+                time2Adj[0] = time2Adj[0] + " PM"
+
+            # Case 5,6: start and end times match. AM or PM
             else:
-                ampmTime = fixTime[0].split(" ")
-                time2Adj[y] = ampmTime[0] + ":00 " + ampmTime[1]
-    
-    # Attach AM or PM to the start time, based on case
-    amORpm = time2Adj[1].split(" ")
+                if (amORpm[1] == "AM"):
+                    time2Adj[0] = time2Adj[0] + " AM"
+                else:
+                    time2Adj[0] = time2Adj[0] + " PM"
 
-    time0 = int(time2Adj[0].split(":")[0])
-    time1 = int(amORpm[0].split(":")[0])
-
-    # Case 1: Noon or midnight is a start time.
-    if ( (time0 > time1) and (time0 == 12) ):
-        if amORpm[1] == "AM":
-            time2Adj[0] = time2Adj[0] + " AM"
-        else:
-            time2Adj[0] = time2Adj[0] + " PM"
-    
-    # Case 2: Noon or midnight is an end time.
-    elif ( (time0 < time1) and (time1 == 12) ):
-        if amORpm[1] == "AM":
-            time2Adj[0] = time2Adj[0] + " PM"
-        else:
-            time2Adj[0] = time2Adj[0] + " AM"
-    
-    # Case 3: transition period from AM to PM (short timeframes)
-    elif ( (amORpm[1] == "PM") and (time0 > time1) ):
-        time2Adj[0] = time2Adj[0] + " AM"
-    
-    # Case 4: transition period from PM to AM (short timeframes)
-    elif ( (amORpm[1] == "AM") and (time0 > time1) ):
-        time2Adj[0] = time2Adj[0] + " PM"
-
-    # Case 5,6: start and end times match. AM or PM
-    else:
-        if (amORpm[1] == "AM"):
-            time2Adj[0] = time2Adj[0] + " AM"
-        else:
-            time2Adj[0] = time2Adj[0] + " PM"
-
-    return time2Adj
+        return time2Adj
 # End standardize_time
 
 
@@ -225,6 +253,8 @@ def itinerary_generator( event_results_df, maps_results_df, num_events, num_rest
 
     # get time portion of date column
     event_time_list = get_when_from_df( event_time_list, event_rows )
+
+    event_time_list = process_dates(event_time_list)
 
     # get start and end times
     event_time_start, event_time_end = get_start_end_times(event_time_list, event_rows)
@@ -244,27 +274,22 @@ def itinerary_generator( event_results_df, maps_results_df, num_events, num_rest
     event_results_df = event_results_df.sort_values(by='start_time')
     
     event_num_list    = [*range(1, event_rows, 1)]
-
-    try:
         
-        # Choose num_restaurants random restaurants for itinerary
-        rand_rest_df  = maps_results_df.sample(num_restaurants)
+    # Choose num_restaurants random restaurants for itinerary
+    rand_rest_df  = maps_results_df.sample(num_restaurants)
 
-        # Choose num_events random events for itinerary
-        rand_event_df = get_event_itinerary( event_results_df, num_events, itin_start_time )
-        # just the time, no date for these columns
-        rand_event_df['start_time'] = pd.to_datetime(rand_event_df['start_time'], format = "%H:%M").dt.time
-        rand_event_df['end_time']   = pd.to_datetime(rand_event_df['end_time'],   format = "%H:%M").dt.time
+    # Choose num_events random events for itinerary
+    rand_event_df = get_event_itinerary( event_results_df, num_events, itin_start_time )
+    # just the time, no date for these columns
+    rand_event_df['start_time'] = pd.to_datetime(rand_event_df['start_time'], format = "%H:%M").dt.time
+    rand_event_df['end_time']   = pd.to_datetime(rand_event_df['end_time'],   format = "%H:%M").dt.time
 
-        
-        event_results_WO_time_df['start_time'] = pd.to_datetime(event_results_WO_time_df['start_time'], format = "%H:%M").dt.time
-        event_results_WO_time_df['end_time']   = pd.to_datetime(event_results_WO_time_df['end_time'],   format = "%H:%M").dt.time
-        
-  
-        return rand_event_df, rand_rest_df, event_results_WO_time_df
-            
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    
+    event_results_WO_time_df['start_time'] = pd.to_datetime(event_results_WO_time_df['start_time'], format = "%H:%M").dt.time
+    event_results_WO_time_df['end_time']   = pd.to_datetime(event_results_WO_time_df['end_time'],   format = "%H:%M").dt.time
+    
+
+    return rand_event_df, rand_rest_df, event_results_WO_time_df    
 # end def itinerary_generator
 
 
@@ -301,6 +326,7 @@ distance = st.slider(
     1, 25
 )
 
+# FIXME - need to process input to accept time written in any format.
 # Get time user would like to start.
 itin_start_time = st.text_input('Enter a time to start for a random itinerary (time format: HH:MM AM/PM. ex: 10:00 AM)')
 
@@ -317,18 +343,23 @@ if st.button("Search my Events ", type="primary"):
     try:
         # Perform the event search using SerpAPI
         event_query = f"events on {formatted_date} in {location_input}"
-        event_params = {
-            "q": event_query,
-            "engine": "google_events",
-            "api_key": "6f2ffa3e0da4d2ac0282279b256693f7615db7d0bb68b042a11547184850bb95",  # Replace with your SerpAPI key
-            "num_results": 100  # Adjust the number of results as needed
-        }
+        events_results = []
+        for page in range(2):
+            event_params = {
+                "q": event_query,
+                "engine": "google_events",
+                "hl": "en",
+                "gl": "us",
+                "api_key": "6f2ffa3e0da4d2ac0282279b256693f7615db7d0bb68b042a11547184850bb95",  # Replace with your SerpAPI key
+                "start": page*10
+            }
 
-        event_search = GoogleSearch(event_params)
-        event_results = event_search.get_dict()
+            event_search = GoogleSearch(event_params)
+            event_results = event_search.get_dict()
 
-        # Extract relevant information from the event results
-        events_results = event_results.get("events_results", [])
+            # Extract relevant information from the event results
+            event_results = event_results.get("events_results", [])
+            events_results = events_results + event_results
 
         if events_results:
             # Construct a DataFrame from events_results
@@ -378,6 +409,8 @@ if st.button("Search my Events ", type="primary"):
         num_restaurants = 3                     # num of restaurants to randomly select and offer as suggestions
         
         itin_start_time = datetime.datetime.strptime(itin_start_time, '%I:%M %p')
+
+        generated_itineray_events, random_restaurants, event_results_WO_time_data_df = itinerary_generator( event_results_df, maps_results_df, num_events, num_restaurants, itin_start_time  )
 
     except Exception as e:
         st.error(f"An error occurred: {e}")
